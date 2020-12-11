aa308ee3ccf0bd1966f61a860f441a72f45ca7d5
raj-vora <raj.jv@somaiya.edu>
2020-12-11 14:47:51 +0530

    code corrections and updates 11/12

--- app/src/main/java/com/rajvora/connect3/MainActivity.java
+++ app/src/main/java/com/rajvora/connect3/MainActivity.java
@@ -17,17 +17,14 @@ public class MainActivity extends AppCompatActivity {
     boolean gameIsActive = true;
     // 2 means unplayed
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
-    int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8},
-                                {0,3,6}, {1,4,7}, {2,5,8},
-                                {0,4,8}, {2,4,6}};
+    int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
 
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if(gameState[tappedCounter] == 2 && gameIsActive == true) {
+        if (gameState[tappedCounter] == 2 && gameIsActive) {
             gameState[tappedCounter] = activePlayer;
-
             counter.setTranslationY(-1000f);
             if (activePlayer == 0) {
                 counter.setImageResource(R.drawable.yellow);
@@ -36,34 +33,34 @@ public class MainActivity extends AppCompatActivity {
                 counter.setImageResource(R.drawable.red);
                 activePlayer = 0;
             }
-        }
-        counter.animate().translationYBy(1000f).rotation(360).setDuration(500);
 
-        for(int[] winningPosition : winningPositions) {
-            if(gameState[winningPosition[0]]==gameState[winningPosition[1]] &&
-                    gameState[winningPosition[1]]==gameState[winningPosition[2]] &&
-                    gameState[winningPosition[0]] !=2) {
-                //Someone has won!!
-                gameIsActive = false;
-                TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
-                if(gameState[winningPosition[0]] == 0)
-                    winnerMessage.setText("Yellow has won!");
-                else
-                    winnerMessage.setText("Red has won!");
-                LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
-                layout.setVisibility(View.VISIBLE);
-            } else {
-                boolean gameIsOver = true;
-                for(int counterState : gameState) {
-                    if(counterState == 2) gameIsOver = false;
-                }
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
 
-                if(gameIsOver) {
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[0]] != 2) {
+                    // Someone has won!
                     gameIsActive = false;
+                    String winner = "Red";
+                    if (gameState[winningPosition[0]] == 0) {
+                        winner = "Yellow";
+                    }
                     TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
-                    winnerMessage.setText("It's a draw");
-                    LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                    winnerMessage.setText(winner + " has won!");
+                    LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
                     layout.setVisibility(View.VISIBLE);
+                } else {
+                    boolean gameIsOver = true;
+                    for (int counterState : gameState) {
+                        if (counterState == 2) gameIsOver = false;
+                    }
+                    if (gameIsOver) {
+                        TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+                        winnerMessage.setText("It's a draw");
+                        LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+                        layout.setVisibility(View.VISIBLE);
+                    }
                 }
             }
         }
@@ -71,17 +68,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void playAgain(View view) {
         gameIsActive = true;
-        //Remove the pop-up
-        LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+        LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
         layout.setVisibility(View.INVISIBLE);
-        //Yellow plays first
         activePlayer = 0;
-        //Set gameState back to original
         Arrays.fill(gameState, 2);
-
-        // Remove all counters
-        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
-        for(int i=0; i<gridLayout.getChildCount(); i++) {
+        GridLayout gridLayout = (GridLayout)findViewById(R.id.gridLayout);
+        for (int i = 0; i< gridLayout.getChildCount(); i++) {
             ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
         }
     }
